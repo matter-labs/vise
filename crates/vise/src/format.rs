3f78c5009300fea69f6fb87e7979c6920a8ed663
Alex Ostrovski <aov@matterlabs.dev>
2023-09-22 10:56:59 +0300

    Test formats with real metrics

--- crates/vise/src/format.rs
+++ crates/vise/src/format.rs
@@ -126,6 +126,7 @@ mod tests {
     use std::fmt::Write as _;
 
     use super::*;
+    use crate::{Counter, Gauge, LabeledFamily, Metrics, Registry, Unit};
 
     #[test]
     fn translating_open_metrics_format() {
@@ -192,5 +193,99 @@ mod tests {
         assert_eq!(buffer, expected);
     }
 
-    // FIXME: test defining real metrics
+    #[derive(Debug, Metrics)]
+    #[metrics(crate = crate, prefix = "test")]
+    pub(crate) struct TestMetrics {
+        /// Test counter.
+        counter: Counter,
+        #[metrics(labels = ["method"])]
+        family_of_counters: LabeledFamily<&'static str, Counter>,
+        #[metrics(unit = Unit::Bytes)]
+        gauge: Gauge<usize>,
+        #[metrics(labels = ["code"])]
+        family_of_gauges: LabeledFamily<u16, Gauge>,
+    }
+
+    #[test]
+    fn translating_real_metrics() {
+        let test_metrics = TestMetrics::default();
+        let mut registry = Registry::empty();
+        registry.register_metrics(&test_metrics);
+
+        test_metrics.counter.inc_by(3);
+        test_metrics.family_of_counters[&"call"].inc_by(5);
+        test_metrics.family_of_counters[&"send_tx"].inc_by(2);
+        test_metrics.gauge.set(42);
+        test_metrics.family_of_gauges[&200].set(12);
+        test_metrics.family_of_gauges[&404].set(4);
+
+        let common_expected_lines = [
+            "# HELP test_counter Test counter.",
+            "# TYPE test_counter counter",
+            "# TYPE test_family_of_counters counter",
+            "# TYPE test_gauge_bytes gauge",
+            "# UNIT test_gauge_bytes bytes",
+            "test_gauge_bytes 42",
+            "# TYPE test_family_of_gauges gauge",
+            "test_family_of_gauges{code=\"200\"} 12",
+            "test_family_of_gauges{code=\"404\"} 4",
+        ];
+        let om_expected_lines = [
+            "test_counter_total 3",
+            "test_family_of_counters_total{method=\"call\"} 5",
+            "test_family_of_counters_total{method=\"send_tx\"} 2",
+        ];
+        let prom_expected_lines = [
+            "test_counter 3",
+            "test_family_of_counters{method=\"call\"} 5",
+            "test_family_of_counters{method=\"send_tx\"} 2",
+        ];
+        let eof = "# EOF";
+
+        assert_format(
+            &registry,
+            Format::OpenMetrics,
+            common_expected_lines
+                .into_iter()
+                .chain(om_expected_lines)
+                .chain([eof]),
+            prom_expected_lines.into_iter(),
+        );
+
+        for prom_format in [Format::OpenMetricsForPrometheus, Format::Prometheus] {
+            let expect_eof = matches!(prom_format, Format::OpenMetricsForPrometheus);
+            let expected_lines = common_expected_lines
+                .into_iter()
+                .chain(prom_expected_lines)
+                .chain(expect_eof.then_some(eof));
+            let missing_lines = om_expected_lines
+                .into_iter()
+                .chain((!expect_eof).then_some(eof));
+            assert_format(&registry, prom_format, expected_lines, missing_lines);
+        }
+    }
+
+    fn assert_format<'a>(
+        registry: &Registry,
+        format: Format,
+        expected_lines: impl Iterator<Item = &'a str>,
+        missing_lines: impl Iterator<Item = &'a str>,
+    ) {
+        println!("Testing {format:?}");
+        let mut buffer = String::new();
+        registry.encode(&mut buffer, format).unwrap();
+        let lines: Vec<_> = buffer.lines().collect();
+        for expected_line in expected_lines {
+            assert!(
+                lines.contains(&expected_line),
+                "Line `{expected_line}` is missing: {lines:#?}"
+            );
+        }
+        for missing_line in missing_lines {
+            assert!(
+                !lines.contains(&missing_line),
+                "Line `{missing_line}` is present: {lines:#?}"
+            );
+        }
+    }
 }
