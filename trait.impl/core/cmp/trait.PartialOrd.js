478263cb0a2c7ffa64884b6e3c9f9dc382158f94
slowli <slowli@users.noreply.github.com>
2025-02-28 11:36:12 +0000

    Deploying to gh-pages from @ matter-labs/vise@51669f42f60c50b3a521662a4ecd71212a303299 🚀

---
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"vise":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"vise/struct.DurationAsSecs.html\" title=\"struct vise::DurationAsSecs\">DurationAsSecs</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"vise":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"vise/struct.BeforeScrapeError.html\" title=\"struct vise::BeforeScrapeError\">BeforeScrapeError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
