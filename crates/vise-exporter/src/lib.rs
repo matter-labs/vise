57eb0d43fe778f8bca37d218794796c205234fb0
Anton Baliasnikov <aba@matterlabs.dev>
2025-04-14 11:56:39 +0100

    chore: fix version in documentation

--- crates/vise-exporter/src/lib.rs
+++ crates/vise-exporter/src/lib.rs
@@ -43,7 +43,7 @@
 //! ```
 
 // Documentation settings.
-#![doc(html_root_url = "https://docs.rs/vise-exporter/0.2.0")]
+#![doc(html_root_url = "https://docs.rs/vise-exporter/0.3.0")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 // Linter settings.
 #![warn(missing_debug_implementations, missing_docs, bare_trait_objects)]
