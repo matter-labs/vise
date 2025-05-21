f5f4f18324562efe07543c2d98669035f99f81ed
slowli <slowli@users.noreply.github.com>
2025-05-21 08:42:53 +0000

    Deploying to gh-pages from @ matter-labs/vise@8c64392753ab8fe563c6fa3db5e4d987952ad011 🚀

---
+++ trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"vise":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"vise/struct.DurationAsSecs.html\" title=\"struct vise::DurationAsSecs\">DurationAsSecs</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/prometheus_client/collector/trait.Collector.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"vise":[["impl&lt;M: <a class=\"trait\" href=\"vise/trait.Metrics.html\" title=\"trait vise::Metrics\">Metrics</a>&gt; Collector for &amp;'static <a class=\"struct\" href=\"vise/struct.Collector.html\" title=\"struct vise::Collector\">Collector</a>&lt;M&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
